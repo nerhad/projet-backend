5444c68d59a5815e3aef95eaa029be31d353f514
Nerhad <nerhad.dilji@outlook.fr>
2021-03-08 00:40:00 +0100

    fix

--- main.js
+++ main.js
@@ -1,23 +1,37 @@
 const url = "http://localhost:90/gsb/praticien";
 
 
-fetch(url + "?ville=Marseille").then(response => response.json()).then((datas) => {
-    datas.forEach((element) => {
+fetch(url).then(response => response.json()).then((data) => {
+    data.forEach((element) => {
 
-      console.log(element)
-
-      if(element.adresse === 'Marseille'){
-        console.log('praticiens de Marseille ' + element.nom + ' ' + element.adresse + ' ' + element.prenom + ' ' + element.specialite);
+      if(element.adresse === 'Marseille') {
+        console.log("error");
       }
+      else{
+        console.log('praticiens de Marseille '+ [element.nom + ' ' + element.adresse + ' ' + element.prenom + ' ' + element.specialite])
+      }
+      
       if(element.adresse === 'Paris' && element.specialite === 'Dentiste'){
-        console.log('dentiste de paris ' + element.nom + ' ' + element.adresse);
+        console.log('error');
       }
+      else{
+        console.log('dentiste de paris ' + element.nom + ' ' + element.adresse)
+      }
+
       if(element.isVisite === true){
-        console.log('praticiens est visite ' + element.nom + ' ' + element.prenom + ' ' + element.specialite + ' ' + element.isVisite);
+        console.log("error");
       }
-      if(element.adresse === 'Mareille' && element.specialite === 'Dentiste' && element.isVisite === true){
-        console.log('dentiste de marseille qui ont été visité ' + element.nom + ' ' + element.adresse + ' ' + element.specialite + ' ' + element.isVisite);
+      else{
+        console.log('praticiens est visite ' + element.nom + ' ' + element.prenom + ' ' + element.specialite + ' ' + element.visite)
       }
+
+      if(element.adresse === 'Mareille' && element.specialite === 'Dentiste' && element.visite === true){
+        console.log('error');
+      }
+      else{
+        console.log('dentiste de marseille qui ont été visité ' + element.nom + ' ' + element.adresse + ' ' + element.specialite + ' ' + element.visite);
+      }
+
   });
 });
 
